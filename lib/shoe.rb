50e98ab23707a4c277e5111445c6436ef3488b19
Richard Cho <richardcho100@gmail.com>
2018-05-02 23:23:07 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,5 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
@@ -9,40 +10,40 @@ class Shoe
   end
 
 
-  def color= (color)
-    @color = color
-  end
-
-  def color
-    @color
-  end
-
-
-  def size= (size)
-    @size = size
-  end
-
-  def size
-    @size
-  end
-
-
-  def material= (material)
-    @material = material
-  end
-
-  def material
-    @material
-  end
-
-
-  def condition= (condition)
-    @condition = condition
-  end
-
-  def condition
-    @condition
-  end
+  # def color= (color)
+  #   @color = color
+  # end
+  #
+  # def color
+  #   @color
+  # end
+  #
+  #
+  # def size= (size)
+  #   @size = size
+  # end
+  #
+  # def size
+  #   @size
+  # end
+  #
+  #
+  # def material= (material)
+  #   @material = material
+  # end
+  #
+  # def material
+  #   @material
+  # end
+  #
+  #
+  # def condition= (condition)
+  #   @condition = condition
+  # end
+  #
+  # def condition
+  #   @condition
+  # end
 
 
   def cobble
