23ae43cc3923044aec0287e274db5b60f0385528
Richard Cho <richardcho100@gmail.com>
2018-05-02 23:24:25 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,5 @@
 class Shoe
+
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
@@ -6,47 +7,9 @@ class Shoe
     @brand = brand
   end
 
-
-
-  # def color= (color)
-  #   @color = color
-  # end
-  #
-  # def color
-  #   @color
-  # end
-  #
-  #
-  # def size= (size)
-  #   @size = size
-  # end
-  #
-  # def size
-  #   @size
-  # end
-  #
-  #
-  # def material= (material)
-  #   @material = material
-  # end
-  #
-  # def material
-  #   @material
-  # end
-  #
-  #
-  # def condition= (condition)
-  #   @condition = condition
-  # end
-  #
-  # def condition
-  #   @condition
-  # end
-
-
   def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"
   end
 
-end  # Make your shoe class here!
+end 
